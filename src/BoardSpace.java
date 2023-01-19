2472b12a8c2576dac4338bd6d9101560709d5e69
Benjamin Frazer <105686628+bfrazer2@users.noreply.github.com>
2023-01-19 12:19:12 -0500

    ref: Changed instance variabels in BoardSpace to private for proper encapsulation

--- src/BoardSpace.java
+++ src/BoardSpace.java
@@ -1,14 +1,14 @@
 public class BoardSpace {
 
-    public String name;
-    public String type;
-    public String owner;
-    public int price;
+    private String name;
+    private String type;
+    private String owner;
+    private int price;
     private int houseCost;
-    public int mortgageValue;
+    private int mortgageValue;
     private int numHouses;
-    public int[] rent;
-    public boolean hasHotel;
+    private int[] rent;
+    private boolean hasHotel;
 
     public BoardSpace(String name, String type, String owner, int price, int houseCost, int mortgageValue, int numHouses, int[] rent, boolean hasHotel) {
         this.name = name;
